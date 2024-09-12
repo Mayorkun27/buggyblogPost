5cddc3e34c56c8409d390ca23a5826386323b9a2
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2024-09-12 09:58:49 +0100

    Unignored the font and resources folder
    also made more changes...Will soon be done

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
 // OPENING AND CLOSING THE NOTIFICATION, CALENDAR AND MENU
 let davi = document.querySelectorAll(".davi");
-let noti = document.querySelectorAll(".not-icon");
-let cal = document.querySelectorAll(".cal-icon");
+
 davi.forEach(element => {
     element.addEventListener("click",()=>{
         const content = document.querySelector(".menu-fl")
-        const content2 = document.querySelector(".notification")
-    
+
         const expanded = content.style.display === 'flex';
-        const expanded2 = content.style.display === 'none';
     
         // Toggle the selected card
         content.style.display = expanded ? 'none' : 'flex';
-        content2.style.display = expanded2 ? 'none' : 'none';
     })
 });
-noti.forEach(element => { 
-    element.addEventListener("click",()=>{
-        const content = document.querySelector(".notification")
-        const content2 = document.querySelector(".menu-fl")
+
+
+// FUNCTION FOR REVEALING AND HIDING PASSWORD
     
-        const expanded = content.style.display === 'flex';
-        const expanded2 = content.style.display === 'none';
-    
-        // Toggle the selected card
-        content.style.display = expanded ? 'none' : 'flex';
-        content2.style.display = expanded2 ? 'none' : 'none';
-      
-    })
-});
-cal.forEach(element => {  
-    element.addEventListener("click",()=>{
-        const content = document.querySelector(".menu-fl")
-        const content2 = document.querySelector(".notification")
-    
-        const expanded = content.style.display === 'none';
-        const expanded2 = content.style.display === 'none';
-    
-        // Toggle the selected card
-        content.style.display = expanded ? 'none' : 'none';
-        content2.style.display = expanded2 ? 'none' : 'none';
-        const infosBar = document.querySelector(".info")
-        infosBar.classList.toggle("changePost");
-        infosBar.classList.toggle("hidden");
-      
-    });
-});
+function togglePasswordVisibility(inputId, fa) {
+    const input = document.getElementById(inputId);
+    const visibility = document.getElementById(fa);
+    input.type === "password" ? input.type = "text" : input.type = "password";
+    if (input.type === "password") {
+        visibility.classList.toggle("ti-eye");
+        visibility.classList.toggle("ti-eye-off");
+    } else {
+        visibility.classList.toggle("ti-eye-off");
+        visibility.classList.toggle("ti-eye");
+    }
+}
